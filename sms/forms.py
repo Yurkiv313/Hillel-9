bffcf3650fd57519d2d4e23fca1809a5bdb21882
Andriy Yurkiv <andriy.yurkiv@gmail.com>
2023-10-29 23:03:19 +0200

    fix hw

--- sms/forms.py
+++ sms/forms.py
@@ -5,6 +5,3 @@ from sms.tasks import send_sms
 class SendSMSForm(forms.Form):
     phone_number = forms.CharField(label='Номер телефону', max_length=14)
     message = forms.CharField(label='Меседж', max_length=160)
-
-    def send_sms(self):
-        send_sms.delay(self.cleaned_data['phone_number'], self.cleaned_data['message'])
